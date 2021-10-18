8d7288753e463301a9573cf117067d5e64afa855
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-18 23:57:19 +0000

    please5

--- led-pwm.py
+++ led-pwm.py
@@ -3,13 +3,13 @@ import cgi
 import json
 data = cgi.FieldStorage()
 s1 = data.getvalue('slider1')
-L = 1
+L = data.getvalue('LED')
 if ('LED1' in data): # changed from OFF to ON
-  L = 1
+#  L = 1
 elif ('LED2' in data) : # changed from ON to OFF
-  L = 2
+#  L = 2
 elif ('LED3' in data) : # changed from ON to OFF
-  L = 3
+#  L = 3
 
 slide = {"slider1":s1, "Le":L}
 with open('led-pwm.txt', 'w') as f:
@@ -26,9 +26,9 @@ with open('led-pwm.txt', 'w') as f:
 print('Content-type: text/html\n\n')
 print('<html>')
 print('<form action="/cgi-bin/led-pwm.py" method="POST">')
-print('<input type="radio" name="LED1" value="a"> LED 1 <br>')
-print('<input type="radio" name="LED2" value="b"> LED 2 <br>')
-print('<input type="radio" name="LED3" value="c"> LED 3 <br>')
+print('<input type="radio" name="LED" value="a"> LED 1 <br>')
+print('<input type="radio" name="LED" value="b"> LED 2 <br>')
+print('<input type="radio" name="LED" value="c"> LED 3 <br>')
 print('<input type="range" name="slider1" min="0" max="100" value="%s"><br>' % s1)
 print('<input type="submit" value="Change LED brightness">')
 print('</form>')
--- led-pwm_background.py
+++ led-pwm_background.py
@@ -29,13 +29,13 @@ while True:
     data = json.load(f)
     dutyCycle = float(data['slider1'])
  
-if data['Le'] == 1:
+if data['Le'] == 'a':
   pwm1.ChangeDutyCycle(dutyCycle)
   time.sleep(0.1)
-if data['Le'] == 2:
+if data['Le'] == 'b':
   pwm2.ChangeDutyCycle(dutyCycle)
   time.sleep(0.1)
-if data['Le'] == 3:
+if data['Le'] == 'c':
   pwm3.ChangeDutyCycle(dutyCycle)
   time.sleep(0.1)
 
