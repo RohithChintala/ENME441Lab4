8661690a77ead1e5602ab9ed469d990a745af985
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-18 19:44:15 +0000

    r

--- lab4_background.py
+++ lab4_background.py
@@ -20,27 +20,31 @@ GPIO.setup(ledPin3, GPIO.OUT)
 pwm1 = GPIO.PWM(ledPin1, 100) # PWM object on our pin at 100 Hz
 pwm1 = GPIO.PWM(ledPin2, 100) # PWM object on our pin at 100 Hz
 pwm3 = GPIO.PWM(ledPin3, 100) # PWM object on our pin at 100 Hz
+pwm1.start(0) 
+pwm2.start(0) 
+pwm3.start(0) 
 
 while True:
  # with open("led_pwm.txt", 'r') as f:
   #  dutyCycle = float(f.read()) # read duty cycle value from file
-  pwm.ChangeDutyCycle(dutyCycle)
-  time.sleep(0.1)
+ # pwm.ChangeDutyCycle(dutyCycle)
+ # time.sleep(0.1)
 
   with open('led-pwm-multiple.txt', 'r') as f:
     data = json.load(f)
+  dutyCycle = float(data['slider1'])
   if data[L] == 1:
-    pwm1.start(0) 
+    pwm1.ChangeDutyCycle(dutyCycle)
   if data[L] == 2:
-    pwm2.start(0) 
+    pwm2.ChangeDutyCycle(dutyCycle)
   if data[L] == 3:
-    pwm3.start(0) 
+    pwm3.ChangeDutyCycle(dutyCycle)
 
 
 
   #pwm.start(0) # start with LED off
   #print("slider 1 = "+ str(data['slider1']))
   #print("slider 2 = "+ str(data['slider2']))
-  dutyCycle = float(data['slider1'])
-  pwm.ChangeDutyCycle(dutyCycle)
-  time.sleep(0.1)
+  #dutyCycle = float(data['slider1'])
+  #pwm.ChangeDutyCycle(dutyCycle)
+  #time.sleep(0.1)
