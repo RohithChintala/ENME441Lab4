27ab40912687d2a752f8ccf5ed455331827d1094
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-19 20:32:47 +0000

    b

--- led-pwm.py
+++ led-pwm.py
@@ -10,21 +10,15 @@ print('Content-type:text/html\n\n')    # blank line = end of headers
 print('<html>')
 print('<head>')
 print('<title>LED switch test</title>')
-print('<meta http-equiv="refresh" content="30">')  # refresh to update LED state
+print('<meta http-equiv="refresh" content="60">') 
 print('</head>')
 print('<body>')
 print('<div style="width:600px;background:#AAAAFF;border:1px;text-align:center">')
 print('<br>')
 print('<font size="3" color="black" face="helvetica">')
-print('<b>LED switch</b>')
+print('<b>Lab 4 LED PWM </b>')
 print('<br><br>')
 
-#if ('LED1' in data): # changed from OFF to ON
-#  L = 1
-#elif ('LED2' in data) : # changed from ON to OFF
-#  L = 2
-#elif ('LED3' in data) : # changed from ON to OFF
-#  L = 3
 
 slide = {"slider1":s1, "Le":L}
 with open('led-pwm.txt', 'w') as f:
@@ -38,10 +32,10 @@ with open('led-pwm.txt', 'w') as f:
 with open('led-pwm.txt', 'w') as f:  
   f.write(str(s1))
 '''
-print('Content-type: text/html\n\n')
+#print('Content-type: text/html\n\n')
 print('<html>')
 print('<form action="/cgi-bin/led-pwm.py" method="POST">')
-print('<input type="radio" name="LED" value="a" checked> LED 1 <br>')
+print('<input type="radio" name="LED" value="a"> LED 1 <br>')
 print('<input type="radio" name="LED" value="b"> LED 2 <br>')
 print('<input type="radio" name="LED" value="c"> LED 3 <br>')
 print('<input type="range" name="slider1" min="0" max="100" value="%s"><br>' % s1)
